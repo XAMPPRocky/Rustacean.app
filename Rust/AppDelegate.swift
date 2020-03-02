e17f13d75302af5f630c72db4f5c964fec3eb171
Erin Power <xampprocky@gmail.com>
2020-03-02 15:29:38 +0100

    Enable switching between toolchain versions

--- Rust/AppDelegate.swift
+++ Rust/AppDelegate.swift
@@ -7,6 +7,24 @@
 //
 
 import Cocoa
+import Toml
+
+@objc enum ToolchainChannel: Int {
+    case stable
+    case beta
+    case nightly
+
+    var description: String {
+        switch self {
+        case .stable:
+            return "stable"
+        case .beta:
+            return "beta"
+        case .nightly:
+            return "nightly"
+        }
+    }
+}
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -27,7 +45,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             logo.draw(in: dstRect)
             return true
         }
-        
+
         statusItem.button!.image = resizedLogo
         statusItem.button!.action = #selector(createMenu)
     }
@@ -35,15 +53,59 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @objc func createMenu() {
         let menu = NSMenu()
 
+        let stable = NSMenuItem(title: "stable", action: #selector(setToolchainStable), keyEquivalent: "s")
+        let beta = NSMenuItem(title: "beta", action: #selector(setToolchainBeta), keyEquivalent: "b")
+        let nightly = NSMenuItem(title: "nightly", action: #selector(setToolchainNightly), keyEquivalent: "n")
+        
+        let settings = try! Toml(contentsOfFile: "/Users/ep/.rustup/settings.toml")
+        if let default_toolchain = settings.string("default_toolchain") {
+            if default_toolchain.contains("stable") {
+                stable.state = .on
+            } else if default_toolchain.contains("beta") {
+                beta.state = .on
+            } else if default_toolchain.contains("nightly") {
+                nightly.state = .on
+            }
+        }
+
         menu.addItem(NSMenuItem(title: "Preferences", action: #selector(showPreferences), keyEquivalent: "p"))
         menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(title: "Stable", action: nil, keyEquivalent: "s"))
-        menu.addItem(NSMenuItem(title: "Beta", action: nil, keyEquivalent: "b"))
-        menu.addItem(NSMenuItem(title: "Nightly", action: nil, keyEquivalent: "n"))
+        menu.addItem(stable)
+        menu.addItem(beta)
+        menu.addItem(nightly)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Targets", action: nil, keyEquivalent: ""))
         menu.addItem(NSMenuItem.separator())
         menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
 
+        print(settings)
+
         statusItem.menu = menu
+    }
+
+    @objc func setToolchainStable() {
+        setToolchainChannel(.stable)
+    }
+
+    @objc func setToolchainBeta() {
+        setToolchainChannel(.beta)
+    }
+
+    @objc func setToolchainNightly() {
+        setToolchainChannel(.nightly)
+    }
+
+    @objc func setToolchainChannel(_ channel: ToolchainChannel) {
+        let rustupPath = "file:///Users/ep/.cargo/bin/rustup"
+        try! Process.run(URL(string: rustupPath)!, arguments: ["default", "\(channel.description)"], terminationHandler: nil)
+        resetChannelState()
+        statusItem.menu?.item(withTitle: channel.description)?.state = .on
+    }
+    
+    func resetChannelState() {
+        statusItem.menu?.item(withTitle: ToolchainChannel.stable.description)?.state = .off
+        statusItem.menu?.item(withTitle: ToolchainChannel.beta.description)?.state = .off
+        statusItem.menu?.item(withTitle: ToolchainChannel.nightly.description)?.state = .off
     }
 
     @objc func showPreferences() {
